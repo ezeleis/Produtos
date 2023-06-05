d38546cce9cbca8e3018a52004e836fe7150c7ba
ezeleis <67750812+ezeleis@users.noreply.github.com>
2023-06-05 17:39:01 -0300

    feature(produtos):set parameters for Produto class

--- src/Entidades/Produtos.java
+++ src/Entidades/Produtos.java
@@ -2,5 +2,14 @@ package Entidades;
 
 public class Produtos {
     public static class Produto{
+        String nome;
+        Double preco;
+        Integer quantidade;
+
+        Produto(String nome, Double preco, Integer quantidade){
+            this.nome = nome;
+            this.quantidade = quantidade;
+            this.preco  =preco;
+        }
     }
 }
