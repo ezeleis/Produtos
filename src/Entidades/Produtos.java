48fcdd4f291bc013c8d18326dd8ca3f432ce291a
ezeleis <67750812+ezeleis@users.noreply.github.com>
2023-06-05 18:18:46 -0300

    feature(Main):input Produto parameters from user

--- src/Entidades/Produtos.java
+++ src/Entidades/Produtos.java
@@ -6,7 +6,7 @@ public class Produtos {
         Double preco;
         Integer quantidade;
 
-        Produto(String nome, Double preco, Integer quantidade){
+        public Produto(String nome, Double preco, Integer quantidade){
             this.nome = nome;
             this.quantidade = quantidade;
             this.preco  =preco;
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 import Entidades.Produtos;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-     Produtos.Produto produto = new Produtos.Produto();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the product name: ");
+        String nome = sc.nextLine();
+
+        System.out.println("Enter the product price: ");
+        Double preco = sc.nextDouble();
+
+        System.out.println("Enter the product quantity: ");
+        Integer quantidade = sc.nextInt();
+
+        Produtos.Produto produto = new Produtos.Produto(nome, preco, quantidade);
     }
 }
